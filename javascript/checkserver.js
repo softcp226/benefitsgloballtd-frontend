367e6af40526af7fb9fee2fdd2212ef2c575be65
softcp226 <softcp226@gmail.com>
2023-04-26 04:44:22 +0100

    edit landing page

--- javascript/checkserver.js
+++ javascript/checkserver.js
@@ -1,18 +1,23 @@
 (async () => {
   try {
-    const response = await fetch("https://benefitsgloballtd-backend.glitch.me", {
-      method: "POST",
-      headers: { "content-type": "application/json" },
-      body: JSON.stringify({ message: "server is up and running" }),
-    });
+    const response = await fetch(
+      "https://benefitsgloballtd-backend.glitch.me",
+      {
+        method: "POST",
+        headers: { "content-type": "application/json" },
+        body: JSON.stringify({ message: "server is up and running" }),
+      },
+    );
     const result = await response.json();
     console.log(result);
     if (result.error) {
-      alert(result.errMessage);
+      console.log(result);
+      // alert(result.errMessage);
     } else {
       console.log(result.message);
     }
   } catch (error) {
-    alert(error.message);
+    // alert(error.message);
+    console.log(error.message)
   }
 })();
