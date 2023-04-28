3abadb73724d9b10d007366a18836ab8427f764c
softcp226 <105775851+softcp226@users.noreply.github.com>
2023-04-28 21:06:28 +0100

    Update approve_deposit.js

--- admin/javascript/approve_deposit.js
+++ admin/javascript/approve_deposit.js
@@ -57,12 +57,11 @@ document.querySelector("#submit").onclick = () => {
   if (!amount.value) return (amount.style.border = "2px solid red");
   amount.style.border = "2px solid #fff";
   const admin = getCookie("admin");
-  const user=getParam()
+  
   const token = getCookie("admin_token");
   const deposit_request = getParam();
   submit_deposit_approval({
     admin: admin,
-    user:user,
     token: token,
     deposit_request,
     deposit_amount: amount.value,
